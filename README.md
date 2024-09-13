deab8f9610f7e5e4dcdb4c19b79ad5fef9cbd245
kasi <gkim@vng.kr>
2024-09-13 20:34:13 +0900

    [Spring boot] HTTP Status Code 제어

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Spring boot 재점검
+
+ 1. HTTP Status Code 제어
+ - POST -> 201 활용체크(ServletUriCompontentsBuild, ResponseEntity)
+ - 
+ - 
--- src/main/java/kr/co/studyconsulting/myrestfulservice/controller/UserController.java
+++ src/main/java/kr/co/studyconsulting/myrestfulservice/controller/UserController.java
@@ -2,8 +2,11 @@ package kr.co.studyconsulting.myrestfulservice.controller;
 
 import kr.co.studyconsulting.myrestfulservice.bean.User;
 import kr.co.studyconsulting.myrestfulservice.dao.UserDaoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController//컨트롤러로 사용할 수 있는 빈 어노테이션
@@ -23,9 +26,17 @@ public class UserController {
     public User retrieveAllUsers(@PathVariable int id) {
         return service.findOne(id);
     }
-    @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+
+    @PostMapping("/users") // 사용자들 요청값을 변환해주기 위해서 ResponseEntity 활용
+    public ResponseEntity<User> createUser(@RequestBody User user){
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
